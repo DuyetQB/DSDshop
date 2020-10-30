8057c2db1fd9c4ffdf07dadede4fe3f274a07c5c
Dinhsiduyet <dinhsyduyet@outlook.com>
2020-10-30 18:53:16 +0700

    myshop1

---
+++ main.js
@@ -0,0 +1,33 @@
+window.onscroll = function(){myScroll()};
+
+let nav = document.querySelector("header");
+let sticky = nav.offsetTop ;
+
+function myScroll(){
+    if(window.pageYOffset >= sticky){
+        nav.classList.add("sticky");
+    }
+    else{
+        nav.classList.remove("sticky");
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded",function(){
+    let btn = document.querySelectorAll(".fas");
+    let content = document.querySelectorAll("nav");
+    let current = "click1";
+   btn[0].onclick = function(){
+       if(current =="click1"){
+           this.style.color ="green";
+           content[0].style.display ="block";
+           current ="click2";
+       }
+       else if(current =="click2"){
+           this.style.color ="black";
+           content[0].style.display ="none";
+           current ="click1";
+       }
+       
+   }
+},false);
